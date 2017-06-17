a8d33169f5ef22dfed37c9005278f07286861b79
Jonathan Matarazzi <jonathanmatarazzi@gmail.com>
2017-06-17 17:28:28 +0200

    removed useless semi-colons

--- p-online.sh
+++ p-online.sh
@@ -1,37 +1,37 @@
-BASEDIR=$(dirname "(dirname "$0")");
+BASEDIR=$(dirname "(dirname "$0")")
 function install() {
     if [ -f $BASEDIR/config ]
     then
-        (cd $BASEDIR/server && exec npm install);
-        (cd $BASEDIR/updater && exec npm install;
+        (cd $BASEDIR/server && exec npm install)
+        (cd $BASEDIR/updater && exec npm install
     else
-        echo "No config file found, create one using the conf command";
+        echo "No config file found, create one using the conf command"
     fi
 }
 function start() {
     if [ -f $BASEDIR/config ]
     then
-        (cd $BASEDIR/server && exec sudo npm start);
+        (cd $BASEDIR/server && exec sudo npm start)
     else
-        echo "No config file found, create one using the conf command";
+        echo "No config file found, create one using the conf command"
     fi
 }
 
 function update() {
     if [ -f $BASEDIR/config ]
     then
-        (cd $BASEDIR/updater && exec sudo npm start);
+        (cd $BASEDIR/updater && exec sudo npm start)
     else
-        echo "No config file found, create one using the conf command";
+        echo "No config file found, create one using the conf command"
     fi
 }
 function conf() {
     if [ -f $BASEDIR/config ]
     then
-        echo "Config file exists, edit $BASEDIR/config to change the configuration.";
+        echo "Config file exists, edit $BASEDIR/config to change the configuration."
     else
-        echo "Creating config file.";
-        cp $BASEDIR/super/config $BASEDIR/config;
-        echo "Config file created, edit $BASEDIR/config to change the configuration.";
+        echo "Creating config file."
+        cp $BASEDIR/super/config $BASEDIR/config
+        echo "Config file created, edit $BASEDIR/config to change the configuration."
     fi
 }
