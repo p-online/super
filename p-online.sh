b24e3d8ef09d2d626b0f60a389b44a13c1737ea1
Jonathan Matarazzi <jonathanmatarazzi@gmail.com>
2017-06-17 17:16:50 +0200

    Added config implementation
    Now checks if config file exists and if not tells you to create one using the new conf command.

--- p-online.sh
+++ p-online.sh
@@ -1,17 +1,37 @@
+BASEDIR=$(dirname ""(dirname "$0")");
 function install() {
-    cd server;
-    npm install;
-    cd ../updater;
-    npm install;
-    cd ..;
+    if [ -f $BASEDIR/config ]
+    then
+        (cd $BASEDIR/server && exec npm install);
+        (cd $BASEDIR/updater && exec npm install;
+    else
+        echo "No config file found, create one using the conf command";
+    fi
 }
 function start() {
-    cd server;
-    sudo npm start;
+    if [ -f $BASEDIR/config ]
+    then
+        (cd $BASEDIR/server && exec sudo npm start);
+    else
+        echo "No config file found, create one using the conf command";
+    fi
 }
 
 function update() {
-    cd updater;
-    sudo npm start;
-    cd ..;
+    if [ -f $BASEDIR/config ]
+    then
+        (cd $BASEDIR/updater && exec sudo npm start);
+    else
+        echo "No config file found, create one using the conf command";
+    fi
+}
+function conf() {
+    if [ -f $BASEDIR/config ]
+    then
+        echo "Config file exists, edit $BASEDIR/config to change the configuration.";
+    else
+        echo "Creating config file.";
+        cp $BASEDIR/super/config $BASEDIR/config;
+        echo "Config file created, edit $BASEDIR/config to change the configuration.";
+    fi
 }
